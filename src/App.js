2631108c9ff1fb0004e08cd7530a02849b2dab01
Amit Naik <84383602+amit-naik-01@users.noreply.github.com>
2022-07-15 10:42:17 +0530

    methods

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+constructor(props) {
+  console.log('constructor')
+	super(props);
+	
+	this.state = {
+    counter:0,
+    delete: false,
+	}
+
+	this.increment = () => this.setState({counter:this.state.counter + 1})
+  this.decrement = () => this.setState({counter:this.state.counter - 1})
+  this.delete = () => this.setState({delete: true })
+}
+
+// static getDerivedStateFromProps(props, state){
+//   return state
+// }
+
+componentDidMount(){
+  console.log('component did mount')
+  console.log("-----xx-----")
+  console.log("-----xx-----")
+}
+
+
+getSnapshotBeforeUpdate(prevProps, prevState){
+  return console.log("Previous State before updating:") +
+   console.log(prevState)
+}
+
+
+
+shouldComponentUpdate(nextProps, nextState){
+  if(nextState !== this.state){
+    console.log("state changed, need to re-render")
+    return true
+  }else{
+  console.log("state did'nt changed,no need to re-render")
+  return false
+}
+}
+
+
+
+componentDidUpdate(prevProps, prevState) {
+  if (prevState !== this.state) {
+    console.log("----xx----")
+    console.log("counter state updated")
+    console.log("----xx----")
+    console.log("----xx----")
+  }
+}
+
+// componentWillUnmount() {
+//   if(this.state.delete === true )
+//   return console.log('Unmounted counter successfully');
+// }
+
+render() {
+  console.log('render happens')
+	return (
+	<div className="App">
+          <div>
+            <button className ="buttons" onClick={this.delete}>Delete the counter component</button>
+            <button className ="buttons" onClick={this.increment}>Increment</button>
+            <button className ="buttons"  onClick={this.decrement}>Decrement</button>
+          </div>
+          <div className="counter">
+              Counter: {this.state.delete ? null : this.state.counter}
+          </div>
+	</div>
+	);
+}
 }
 
 export default App;
